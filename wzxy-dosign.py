fad249a2be757f26c27aaa6af6c162c1d893d41f
Minelu <34987057+Minelu@users.noreply.github.com>
2021-09-28 23:11:03 +0800

    Update wzxy-dosign.py

--- wzxy-dosign.py
+++ wzxy-dosign.py
@@ -77,35 +77,52 @@ class WoZaiXiaoYuanPuncher:
     # 获取打卡列表，判断当前打卡时间段与打卡情况，符合条件则自动进行打卡
     def PunchIn(self):
         print("获取打卡列表中...")
+
+
+        url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
+        self.header['Host'] = "student.wozaixiaoyuan.com"
+        self.header['Content-Type'] = "application/x-www-form-urlencoded"
+        self.header['JWSESSION'] = self.getJwsession()
+        sim_data = {
+            "page": 1,
+            "size": 5
+        }
+        data = urlencode(sim_data)
+        self.session = requests.session()    
+        response = self.session.post(url=url, data=data, headers=self.header)
+        response = json.loads(response.text)
+        print(response)
+
+
+        # # url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
+        # # self.header['Host'] = "student.wozaixiaoyuan.com"
+        # # self.header['JWSESSION'] = self.getJwsession()
         # url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
+        # ## values = {'page=1&size=5'}
         # self.header['Host'] = "student.wozaixiaoyuan.com"
         # self.header['JWSESSION'] = self.getJwsession()
-        url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
-        ## values = {'page=1&size=5'}
-        self.header['Host'] = "student.wozaixiaoyuan.com"
-        self.header['JWSESSION'] = self.getJwsession()
 
-        sims_data = {"page": 1, "size": 5}
-        data = urlencode(sims_data)
-        # 打印values的数据类型,输出<class 'dict'>
-        ## print(type(values))
-        ## print(values)
-        # json.dump将python对象编码成json字符串
-        ##values_json = json.dumps(values)
-        # 打印编码成json字符串的values_json的数据类型,输出<class 'str'>
-        ##print(type(values_json))
-        ##print(values_json)
-        # requests库提交数据进行post请求
-        ##req = requests.post(url, data=values_json, headers=self.header)
-        req = requests.post(url, data=data, headers=self.header)
-        # 打印Unicode编码格式的json数据
-        print(req.text)
-        # 使用json.dumps()时需要对象相应的类型是json可序列化的
-        change = req.json()
-        # json.dumps序列化时对中文默认使用ASCII编码,如果无任何配置则打印的均为ascii字符,输出中文需要指定ensure_ascii=False
-        new_req = json.dumps(change, ensure_ascii=False)
-        # 打印接口返回的数据,且以中文编码
-        print(new_req)
+        # sims_data = {"page": 1, "size": 5}
+        # data = urlencode(sims_data)
+        # # 打印values的数据类型,输出<class 'dict'>
+        # ## print(type(values))
+        # ## print(values)
+        # # json.dump将python对象编码成json字符串
+        # ##values_json = json.dumps(values)
+        # # 打印编码成json字符串的values_json的数据类型,输出<class 'str'>
+        # ##print(type(values_json))
+        # ##print(values_json)
+        # # requests库提交数据进行post请求
+        # ##req = requests.post(url, data=values_json, headers=self.header)
+        # req = requests.post(url, data=data, headers=self.header)
+        # # 打印Unicode编码格式的json数据
+        # print(req.text)
+        # # 使用json.dumps()时需要对象相应的类型是json可序列化的
+        # change = req.json()
+        # # json.dumps序列化时对中文默认使用ASCII编码,如果无任何配置则打印的均为ascii字符,输出中文需要指定ensure_ascii=False
+        # new_req = json.dumps(change, ensure_ascii=False)
+        # # 打印接口返回的数据,且以中文编码
+        # print(new_req)
 
 
 

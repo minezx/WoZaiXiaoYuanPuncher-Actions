337e4a1de0301134b1fddd338963d8786835aae0
Minelu <34987057+Minelu@users.noreply.github.com>
2021-09-28 22:54:02 +0800

    Update wzxy-dosign.py

--- wzxy-dosign.py
+++ wzxy-dosign.py
@@ -81,19 +81,20 @@ class WoZaiXiaoYuanPuncher:
         # self.header['Host'] = "student.wozaixiaoyuan.com"
         # self.header['JWSESSION'] = self.getJwsession()
         url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
-        values = {'page=1&size=5'}
+        ## values = {'page=1&size=5'}
         self.header['Host'] = "student.wozaixiaoyuan.com"
         self.header['JWSESSION'] = self.getJwsession()
         # 打印values的数据类型,输出<class 'dict'>
-        print(type(values))
-        print(values)
+        ## print(type(values))
+        ## print(values)
         # json.dump将python对象编码成json字符串
-        values_json = json.dumps(values)
+        ##values_json = json.dumps(values)
         # 打印编码成json字符串的values_json的数据类型,输出<class 'str'>
-        print(type(values_json))
-        print(values_json)
+        ##print(type(values_json))
+        ##print(values_json)
         # requests库提交数据进行post请求
-        req = requests.post(url, data=values_json, headers=self.header)
+        ##req = requests.post(url, data=values_json, headers=self.header)
+        req = requests.post(url, data="page=1&size=5", headers=self.header)
         # 打印Unicode编码格式的json数据
         print(req.text)
         # 使用json.dumps()时需要对象相应的类型是json可序列化的

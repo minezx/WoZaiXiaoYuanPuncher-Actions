0abf96853c8b8c457805665e20e6e8b7fa704dc7
Minelu <34987057+Minelu@users.noreply.github.com>
2021-09-29 22:42:59 +0800

    Update wzxy-dosign.py

--- wzxy-dosign.py
+++ wzxy-dosign.py
@@ -143,8 +143,45 @@ class WoZaiXiaoYuanPuncher:
     # 参数seq ： 当前打卡的序号
     def doPunchIn(self,signid,signlogId):
         print("正在测试签到...")
-        print(signid)
-        print(signlogId)
+
+
+
+        url = "https://student.wozaixiaoyuan.com/sign/doSign.json"
+        self.header['Host'] = "student.wozaixiaoyuan.com"
+        self.header['Content-Type'] = "application/json"
+        self.header['JWSESSION'] = self.getJwsession()
+        values = {
+            "id": str(signid),
+            "signId": str(signlogId),
+            "latitude": 34.102702,
+            "longitude": 108.653637,
+            "country": "中国",
+            "province": "陕西省",
+            "city": "西安市",
+            "district": "鄠邑区",
+            "township": "五竹街道"
+        }
+        # 打印values的数据类型,输出<class 'dict'>
+        print(type(values))
+        print(values)
+        # json.dump将python对象编码成json字符串
+        values_json = json.dumps(values)
+        # 打印编码成json字符串的values_json的数据类型,输出<class 'str'>
+        print(type(values_json))
+        print(values_json)
+        # requests库提交数据进行post请求
+        req = requests.post(url=url, data=values_json, headers=self.header)
+        # 打印Unicode编码格式的json数据
+        print(req.text)
+        # 使用json.dumps()时需要对象相应的类型是json可序列化的
+        change = req.json()
+        # json.dumps序列化时对中文默认使用ASCII编码,如果无任何配置则打印的均为ascii字符,输出中文需要指定ensure_ascii=False
+        new_req = json.dumps(change, ensure_ascii=False)
+        # 打印接口返回的数据,且以中文编码
+        print(new_req)
+
+
+
         # url = "https://student.wozaixiaoyuan.com/sign/doSign.json"
         # self.header['Host'] = "student.wozaixiaoyuan.com"
         # self.header['Content-Type'] = "application/json"
